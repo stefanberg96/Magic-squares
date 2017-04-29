33adc3f78823238571b49e6c08db45572513dbfe
stefanberg96 <bergshm@gmail.com>
2017-04-29 18:51:53 +0200

    works up till 15 blocks

--- ParkerSquare2/kernel.cu
+++ ParkerSquare2/kernel.cu
@@ -6,6 +6,9 @@
 #include "CombinationIt.h"
 #include <list>
 
+#define BLOCKS 709;
+#define THREADS 512;
+
 __global__ void addKernel(int * c);
 
 cudaError_t addWithCuda(int *c);
@@ -19,6 +22,24 @@ __device__ void factorial(long * number) {
 		n--;
 		*number *= n;
 	}
+}
+
+__device__ bool magicSquare(int *c) {
+	int r1 = c[0] + c[1] + c[2];
+	int r2 = c[3] + c[4] + c[5];
+	int r3 = c[6] + c[7] + c[8];
+
+	int c1 = c[0] + c[3] + c[6];
+	int c2 = c[1] + c[4] + c[7];
+	int c3 = c[2] + c[5] + c[8];
+
+	int d1 = c[0] + c[4] + c[8];
+	int d2 = c[6] + c[4] + c[2];
+
+	bool temp = r1 == r2 && r2 == r3 && r1 == c1 && c1 == c2 && c2 == c3;
+	temp = temp && (r1 == d1 || r1 == d2);
+
+	return temp;
 }
 
 __device__ void getIth(int *c, int *index) {
@@ -86,18 +107,16 @@ __device__ void initialize(int c[]) {
 
 __global__ void addKernel(int *c,int * factorial)
 {
-	int *test=new int;
-	*test = 8;
-	getPermutation(c,test, factorial);
+	int index = blockDim.x*blockIdx.x + threadIdx.x;
+	int *c2 = &c[index * 9];
+	if (index < 362880) {//ignore cases which are outside of the 9! range
+		getPermutation(c2, &index, factorial);
+	}
 }
 
 int main()
 {
-    const int arraySize = 9;
-
-    int c[arraySize] = { 0 ,1,2,3,4,5,6,7,8};
-	
-
+	int c[9] = { 0,1,2,3,4,5,6,7,8 };
     // Add vectors in parallel.
     cudaError_t cudaStatus = addWithCuda(c);
     if (cudaStatus != cudaSuccess) {
@@ -129,6 +148,20 @@ int main()
 
 cudaError_t addWithCuda(int *c)
 {
+	
+	int t =  BLOCKS;
+	t *= THREADS;
+	int *ccopy = new int[t*9];
+	
+
+	for (int i = 0; i < t; i++) {
+		for (int j = 0; j < 9; j++) {
+			ccopy[i * 9 + j] = c[j];
+		}
+	}
+
+	printf("here");
+
     int *dev_c = 0;
     cudaError_t cudaStatus;
 
@@ -139,6 +172,7 @@ cudaError_t addWithCuda(int *c)
 		factorial[i] = factorial[i - 1] * i;
 	}
 
+	
     // Choose which GPU to run on, change this on a multi-GPU system.
     cudaStatus = cudaSetDevice(0);
     if (cudaStatus != cudaSuccess) {
@@ -147,7 +181,7 @@ cudaError_t addWithCuda(int *c)
     }
 
     // Allocate GPU buffers for three vectors (two input, one output)    .
-    cudaStatus = cudaMalloc((void**)&dev_c,709*512 *9 * sizeof(int));
+    cudaStatus = cudaMalloc((void**)&dev_c,t*9*sizeof(int));
     if (cudaStatus != cudaSuccess) {
         fprintf(stderr, "cudaMalloc failed!");
         goto Error;
@@ -167,14 +201,14 @@ cudaError_t addWithCuda(int *c)
 	}
 
 	// Allocate GPU buffers for three vectors(two input, one output)    .
-	cudaStatus = cudaMemcpy(dev_c, c, 9 * sizeof(int), cudaMemcpyHostToDevice);
+	cudaStatus = cudaMemcpy(dev_c, ccopy, t *9*sizeof(int), cudaMemcpyHostToDevice);
 	if (cudaStatus != cudaSuccess) {
 		fprintf(stderr, "cudaMalloc failed!");
 		goto Error;
 	}
 
     // Launch a kernel on the GPU with one thread for each element.
-    addKernel<<<1, 9>>>(dev_c,d_factorial);
+    addKernel<<<709, 512>>>(dev_c,d_factorial);
 
     // Check for any errors launching the kernel
     cudaStatus = cudaGetLastError();
@@ -192,7 +226,7 @@ cudaError_t addWithCuda(int *c)
     }
 
     // Copy output vector from GPU buffer to host memory.
-    cudaStatus = cudaMemcpy(c, dev_c, 9 * sizeof(int), cudaMemcpyDeviceToHost);
+    cudaStatus = cudaMemcpy(ccopy, dev_c, t*9 * sizeof(int), cudaMemcpyDeviceToHost);
     if (cudaStatus != cudaSuccess) {
         fprintf(stderr, "cudaMemcpy failed!");
         goto Error;
@@ -200,6 +234,7 @@ cudaError_t addWithCuda(int *c)
 
 Error:
     cudaFree(dev_c);
+	cudaFree(d_factorial);
     
     return cudaStatus;
 }
